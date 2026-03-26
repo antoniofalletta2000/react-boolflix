b0165a9bc323cadabcb2420206b3e851a1fe5d23
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-26 10:25:16 +0100

    add link api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@
 
 function App() {
 
+  const API_KEY= import.meta.env.VITE_API_KEY
+  const url =`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=matrix`
+
+  console.log(url);
+  
   return (
     <>
       
