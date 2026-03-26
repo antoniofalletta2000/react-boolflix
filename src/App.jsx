56bbf6ad606b4b65fce51d53ce0602ec2bde7f52
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-26 10:59:57 +0100

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-
+import { useState, useEffect } from "react";
 
 function App() {
+  const [films, setFilms] = useState([])
+  const [typeTitle, setTypeTitle] = useState("")
+  console.log(typeTitle);
 
-  const API_KEY= import.meta.env.VITE_API_KEY
-  const url =`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=matrix`
+  function handleSubmit(e) {
+    e.preventDefault()
+    const API_KEY = import.meta.env.VITE_API_KEY
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${typeTitle}`
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.results);
+        setFilms(data.results)
+      })
+  }
 
-  console.log(url);
-  
   return (
     <>
-      
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={typeTitle} onChange={(e) => setTypeTitle(e.target.value)} />
+        <button type="submit">Cerca</button>
+      </form>
+
+      <ul>
+        {
+          films.map(film => (
+            <li key={film.id}>
+              <div>{film.title}</div>
+              <div>{film.original_title}</div>
+              <div>{film.original_language}</div>
+              <div>{film.vote_average}</div>
+            </li>
+          ))
+        }
+      </ul>
+
     </>
   )
 }
