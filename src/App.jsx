b8ffc2997ae7dbed6bacf94d1d98f4150d5350f6
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-26 15:08:24 +0100

    milestone 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,71 +3,79 @@ import "flag-icons/css/flag-icons.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import FilmStar from "./components/FilmStar";
 import SerieStar from "./components/SerieStar";
+import AppHeader from "./components/AppHeader";
 
 function App() {
   const [films, setFilms] = useState([])
   const [series, setSeries] = useState([])
   const [typeTitle, setTypeTitle] = useState("")
-  console.log(typeTitle);
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    const API_KEY = import.meta.env.VITE_API_KEY
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${typeTitle}`
-    const url_tv =`https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&language=it_IT&query=${typeTitle}`
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        setFilms(data.results)
-        setTypeTitle("")
-      });
-    fetch(url_tv)
-      .then(res => res.json())
-      .then(data => {
-        setSeries(data.results)
-        setTypeTitle("")
-      });
-  }
+
+
 
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={typeTitle} onChange={(e) => setTypeTitle(e.target.value)} />
-        <button type="submit">Cerca</button>
-      </form>
+      <AppHeader setFilms={setFilms} setTypeTitle={setTypeTitle} setSeries={setSeries} typeTitle={typeTitle} />
+      <div className="container">
 
-      <ul>
-        {
-          films.map(film => (
-            <li key={film.id}>
-              <div><img src={`https://image.tmdb.org/t/p/w342/${film.poster_path}`} alt="" /></div>
-              <div>{film.title}</div>
-              <div>{film.original_title}</div>
-              <div className={`fi fi-${film.original_language === "en" ? "gb" : film.original_language === "ja" ? "jp" : film.original_language}`}></div>
-              <div>{Math.ceil(film.vote_average / 2)}</div>
-              <div>
-                <FilmStar film={film}/>
-              </div>
-            </li>
-          ))
-        }
-        {
-          series.map(serie => (
-            <li key={serie.id}>
-              <div><img src={`https://image.tmdb.org/t/p/w342/${serie.poster_path}`} alt="" /></div>
-              <div>{serie.name}</div>
-              <div>{serie.original_name}</div>
-              <div className={`fi fi-${serie.original_language === "en" ? "gb" : serie.original_language === "ja" ? "jp" : serie.original_language}`}></div>
-              <div>{Math.ceil(serie.vote_average / 2)}</div>
-              <div>
-                <SerieStar serie={serie}/>
-              </div>
-            </li>
-          ))
-        }
-      </ul>
+        <ul className="list-unstyled row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 d-flex">
+          {
+            films.map(film => (
+              <li key={film.id} className=" col pt-4">
+                <div className="card bg-dark text-white">
+                  <div className="d-flex justify-content-center">
+                    <img src={`https://image.tmdb.org/t/p/w342/${film.poster_path}`} className="card_poster" alt="" />
+                  </div>
+                  <div className="card-body card_info pt-2">
+                    <div ><span className="fw-bold">Titolo:</span> {film.title}</div>
+                    <div ><span className="fw-bold">Titolo originale:</span> {film.original_title}</div>
+                    <div className="d-flex gap-2">
+                      <span className="fw-bold">Paese d'origine:</span>
+                      <div className={` fi fi-${film.original_language === "en" ? "gb" : film.original_language === "ja" ? "jp" : film.original_language}`}></div>
+                    </div>
 
+                    <div className="d-flex gap-2">
+                      <span className="fw-bold">Voto:</span>
+                      <FilmStar film={film} />
+                    </div>
+                    <div ><span className="fw-bold">Trama:</span> {film.overview}</div>
+                  </div>
+
+                </div>
+              </li>
+            ))
+          }
+
+          {
+            series.map(serie => (
+              <li key={serie.id} className=" col pt-4">
+                <div className="card bg-dark text-white">
+                  <div className="d-flex justify-content-center">
+                    <img src={`https://image.tmdb.org/t/p/w342/${serie.poster_path}`} className="card_poster" alt="" />
+                  </div>
+                  <div className="card-body card_info pt-2">
+                    <div ><span className="fw-bold">Titolo:</span> {serie.name}</div>
+                    <div ><span className="fw-bold">Titolo originale:</span> {serie.original_name}</div>
+                    <div className="d-flex gap-2">
+                      <span className="fw-bold">Paese d'origine:</span>
+                      <div className={` fi fi-${serie.original_language === "en" ? "gb" : serie.original_language === "ja" ? "jp" : serie.original_language}`}></div>
+                    </div>
+
+                    <div className="d-flex gap-2">
+                      <span className="fw-bold">Voto:</span>
+                      <SerieStar serie={serie} />
+                    </div>
+                    <div ><span className="fw-bold">Trama:</span> {serie.overview}</div>
+                  </div>
+
+                </div>
+              </li>
+            ))
+          }
+        </ul >
+
+      </div>
     </>
   )
 }
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,47 @@
+export default function AppHeader({ setFilms, setTypeTitle, setSeries, typeTitle }) {
+    function handleSubmit(e) {
+        e.preventDefault()
+        const API_KEY = import.meta.env.VITE_API_KEY
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${typeTitle}`
+        const url_tv = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&language=it_IT&query=${typeTitle}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setFilms(data.results)
+                setTypeTitle("")
+            });
+        fetch(url_tv)
+            .then(res => res.json())
+            .then(data => {
+                setSeries(data.results)
+                setTypeTitle("")
+            });
+    }
+
+
+    return (
+        <>
+            <div className="bg-dark">
+                <div className="container pt-3 pb-2 ">
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 d-flex justify-content-between align-items-center">
+                        <div className="col">
+                            <h2 className="text-danger">Boolflix</h2>
+                        </div>
+                        <div className="col">
+                            <form onSubmit={handleSubmit} className="d-flex">
+                                <input type="text" className="border-0 rounded-start" value={typeTitle} onChange={(e) => setTypeTitle(e.target.value)} placeholder="Cosa vorresti guardare?" />
+                                <div>
+                                    <button type="submit" className="p-2 border-0 bg-danger text-white rounded-end">Cerca</button>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </>
+    )
+}
